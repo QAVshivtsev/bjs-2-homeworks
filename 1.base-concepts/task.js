7e559100c75aca6cc0c10daf26ca048de9060cbe
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-07-31 20:53:32 +0300

    'Первое задание полностью готово'

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,29 @@
-"use strict"
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+	let arr = [];
+	let discriminant = b ** 2 - 4 * a * c;
+	if (discriminant < 0) {
+		arr = [];
+		return arr;
+	}
+	if (discriminant == 0) {
+		let toEqual1 = -b / (2 * a);
+		arr = [toEqual1];
+		return arr;
+	}
+	if (discriminant > 0) {
+		let toEqual1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+		let toEqual2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+		arr = [toEqual1, toEqual2];
+		return arr;
+	}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+	let interestRate = percent / 100 / 12;
+	let loanBody = amount - contribution;
+	let payment = loanBody * (interestRate + interestRate / ((1 + interestRate) ** countMonths - 1));
+	let totalAmount = +(payment * countMonths).toFixed(2);
+	return totalAmount;
 }
