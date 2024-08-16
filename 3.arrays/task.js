1c3b0f5024baade1725e11b5ef34af7e97aa4b3c
QAVshivtsev <159697104+QAVshivtsev@users.noreply.github.com>
2024-08-16 18:49:18 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,30 @@
 function compareArrays(arr1, arr2) {
+    return (
+      arr1.length === arr2.length &&
+      arr1.every((item, index) => item === arr2[index])
+    );
+  }
   
-}
-
-function getUsersNamesInAgeRange(users, gender) {
+  function getUsersNamesInAgeRange(users, gender) {
+    const filteredUsers = users.filter((user) => user.gender === gender);
+    if (filteredUsers.length === 0) {
+      return 0;
+    }
+    const sum = filteredUsers.reduce((total, user) => total + user.age, 0);
+    return sum / filteredUsers.length;
+  }
   
-}
+  //   function getUsersNamesInAgeRange(users, gender) {
+  //     let result = 0;
+  //     let sum = 0;
+  //     for (let i = 0; i < users.length; i++) {
+  //       if (users[i].gender === gender) {
+  //         sum += users[i].age;
+  //         result += 1;
+  //       }
+  //     }
+  //     if (result === 0) {
+  //       return 0;
+  //     }
+  //     return sum / result;
+  //   }  
